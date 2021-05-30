a3f2b39e5cbf95908973698e8bab24eb6e1b9e30
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-05-30 22:21:17 +0530

    order

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react";
 import Title from "../Heading/Title";
-import Blog from '../Blog/Blog'
-import Segment from '../Ways to Help/segment'
-import Timeline from '../Timeline/Timeline'
+import Blog from "../Blog/Blog";
+import Segment from "../Ways to Help/segment";
+import Timeline from "../Timeline/Timeline";
 import Overview from "../overview/Overview";
 import ContactUs from "../contactUs/ContactUs.js";
 import Footer from "../footer/Footer.js";
 import OurWork from "../ourwork/OurWork";
 
 function Home() {
-    return (
-        <div>
-            <Title />
-            <Segment />
-            <Timeline />
-            <Blog />
-            <Overview />
-            <OurWork />
-            <ContactUs />
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <Title />
+      <Overview />
+      <Timeline />
+      <OurWork />
+      <Blog />
+      <Segment />
+      <ContactUs />
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
