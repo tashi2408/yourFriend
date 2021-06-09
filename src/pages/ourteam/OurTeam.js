548052073da1f8e39ffae826caee4730e717d46e
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-06-09 19:21:47 +0530

    added pages folder

---
+++ src/pages/ourteam/OurTeam.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../../components/footer/Footer";
+import Nav from "../../components/Navbar/Nav";
+
+function OurTeam() {
+  return (
+    <div>
+      <Nav />
+      <div className="ourteam"></div>
+      <Footer />
+    </div>
+  );
+}
+
+export default OurTeam;
