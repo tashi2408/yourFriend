df403e322cae6c4c92f931e8ef771c8dabe599c3
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-06-24 01:13:30 +0530

    addition css links removed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
-import Campaign from "./components/Campaign/Campaign";
+// import Campaign from "./components/Campaign/Campaign";
 import OurTeam from "./components/ourteam/OurTeam";
 import Aboutus from "./components/Aboutus/Aboutus";
-import Webinar from "./components/Webinar/Webinar";
+// import Webinar from "./components/Webinar/Webinar";
 
 function App() {
   return (
     <Router>
       <Route exact path="/" component={Home} />
-      <Route exact path="/campaign" component={Campaign} />
+      {/* <Route exact path="/campaign" component={Campaign} /> */}
       <Route exact path="/ourteam" component={OurTeam} />
       <Route exact path="/about-us" component={Aboutus} />
-      <Route exact path="/webinars" component={Webinar} />
+      {/* <Route exact path="/webinars" component={Webinar} /> */}
     </Router>
   );
 }
