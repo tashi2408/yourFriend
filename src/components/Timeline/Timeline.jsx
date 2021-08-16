d86a4de69eaab225672c109173139553614c1a38
saurabh021120 <saurabhratnaparkhi11@gmail.com>
2021-08-16 11:19:38 +0530

    timeline text added

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -31,7 +31,7 @@ const Timeline = () => {
             <h4 className="vertical-timeline-element-title fontcss">
               Events 1
             </h4>
-            <h4 className="vertical-timeline-element-subtitle">Place</h4>
+            {/* <h4 className="vertical-timeline-element-subtitle">Place</h4> */}
             <p>
               “Break through Obstacles” National webinar with Dr Neelam Mishra
             </p>
@@ -52,7 +52,7 @@ const Timeline = () => {
             <h4 className="vertical-timeline-element-title fontcss fontcss">
               Events 2
             </h4>
-            <h4 className="vertical-timeline-element-subtitle">Place</h4>
+            {/* <h4 className="vertical-timeline-element-subtitle">Place</h4> */}
             <p>Fortis Hospitals Mental Health Month Collaboration</p>
             <button className="work-button">
               <IoArrowRedoCircleSharp className="work-icon" />
@@ -71,9 +71,9 @@ const Timeline = () => {
             <h4 className="vertical-timeline-element-title fontcss">
               Events 3
             </h4>
-            <h4 className="vertical-timeline-element-subtitle">Place</h4>
+            {/* <h4 className="vertical-timeline-element-subtitle">Place</h4> */}
             <p>
-              - Collaborations with Biocom, SRM and Baatien Unkaheesi (mental
+              Collaborations with Biocom, SRM and Baatien Unkaheesi (mental
               health initiative)
             </p>
             <button className="work-button">
@@ -85,7 +85,7 @@ const Timeline = () => {
             className="vertical-timeline-element--work check"
             contentStyle={{ background: "#393b989c", color: "#fff" }}
             contentArrowStyle={{ borderRight: "7px solid  #393b989c" }}
-            date="1st of July"
+            date="August 2020"
             dateClassName={"datecolor"}
             iconStyle={{ background: "#fff", color: "#fff" }}
             icon={<AiOutlineLink className="colorcss" />}
@@ -93,11 +93,8 @@ const Timeline = () => {
             <h4 className="vertical-timeline-element-title fontcss">
               Events 4
             </h4>
-            <h4 className="vertical-timeline-element-subtitle">Place</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incidid
-            </p>
+            {/* <h4 className="vertical-timeline-element-subtitle">Place</h4> */}
+            <p>“Psychology Simplified” - PAN India webinar series</p>
             <button className="work-button">
               <IoArrowRedoCircleSharp className="work-icon" />
             </button>
@@ -107,7 +104,7 @@ const Timeline = () => {
             className="vertical-timeline-element--work check"
             contentStyle={{ background: "#393b989c", color: "#fff" }}
             contentArrowStyle={{ borderRight: "7px solid  #393b989c" }}
-            date="12th of July"
+            date="19th September 2020"
             dateClassName={"datecolor"}
             iconStyle={{ background: "#fff", color: "#fff" }}
             icon={<BiCheck className="colorcss" />}
@@ -115,10 +112,10 @@ const Timeline = () => {
             <h4 className="vertical-timeline-element-title fontcss">
               Events 5
             </h4>
-            <h4 className="vertical-timeline-element-subtitle">Place</h4>
+            {/* <h4 className="vertical-timeline-element-subtitle">Place</h4> */}
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incidid
+              Navratan Foundation Collaboration - mental health session with Mrs
+              Priti Srivastava
             </p>
             <button className="work-button">
               <IoArrowRedoCircleSharp className="work-icon" />
@@ -129,7 +126,7 @@ const Timeline = () => {
             className="vertical-timeline-element--work check"
             contentStyle={{ background: "#393b989c", color: "#fff" }}
             contentArrowStyle={{ borderRight: "7px solid  #393b989c" }}
-            date="1st of August"
+            date="31st October 2020"
             dateClassName={"datecolor"}
             iconStyle={{ background: "#fff", color: "#fff" }}
             icon={<AiOutlineLink className="colorcss" />}
@@ -137,8 +134,93 @@ const Timeline = () => {
             <h4 className="vertical-timeline-element-title fontcss">
               Events 6
             </h4>
-            <h4 className="vertical-timeline-element-subtitle">Place</h4>
-            <p>Change Visible</p>
+            {/* <h4 className="vertical-timeline-element-subtitle">Place</h4> */}
+            <p>“Tackle your Talk” with Sakshi Pasricha </p>
+            <button className="work-button">
+              <IoArrowRedoCircleSharp className="work-icon" />
+            </button>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work check"
+            contentStyle={{ background: "#393b989c", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  #393b989c" }}
+            date="24th October 2020"
+            dateClassName={"datecolor"}
+            iconStyle={{ background: "#fff", color: "#fff" }}
+            icon={<AiOutlineLink className="colorcss" />}
+          >
+            <h4 className="vertical-timeline-element-title fontcss">
+              Events 7
+            </h4>
+            {/* <h4 className="vertical-timeline-element-subtitle">Place</h4> */}
+            <p>“Sought your Thoughts” with Anandita Anand </p>
+            <button className="work-button">
+              <IoArrowRedoCircleSharp className="work-icon" />
+            </button>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work check"
+            contentStyle={{ background: "#393b989c", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  #393b989c" }}
+            date="23rd April 2021"
+            dateClassName={"datecolor"}
+            iconStyle={{ background: "#fff", color: "#fff" }}
+            icon={<AiOutlineLink className="colorcss" />}
+          >
+            <h4 className="vertical-timeline-element-title fontcss">
+              Events 8
+            </h4>
+            {/* <h4 className="vertical-timeline-element-subtitle">Place</h4> */}
+            <p>
+              panel discussion with mental health professionals: Shweta Sharma,
+              Kunashni Parekh, Srishti
+            </p>
+            <button className="work-button">
+              <IoArrowRedoCircleSharp className="work-icon" />
+            </button>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work check"
+            contentStyle={{ background: "#393b989c", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  #393b989c" }}
+            date="26th June 2021"
+            dateClassName={"datecolor"}
+            iconStyle={{ background: "#fff", color: "#fff" }}
+            icon={<AiOutlineLink className="colorcss" />}
+          >
+            <h4 className="vertical-timeline-element-title fontcss">
+              Events 9
+            </h4>
+            {/* <h4 className="vertical-timeline-element-subtitle">Place</h4> */}
+            <p>
+              “YOU 2.0: -Unleashing a better version of yourself” A series of
+              live sessions with mental health activists
+            </p>
+            <button className="work-button">
+              <IoArrowRedoCircleSharp className="work-icon" />
+            </button>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work check"
+            contentStyle={{ background: "#393b989c", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  #393b989c" }}
+            date="31st July 2021"
+            dateClassName={"datecolor"}
+            iconStyle={{ background: "#fff", color: "#fff" }}
+            icon={<AiOutlineLink className="colorcss" />}
+          >
+            <h4 className="vertical-timeline-element-title fontcss">
+              Events 10
+            </h4>
+            {/* <h4 className="vertical-timeline-element-subtitle">Place</h4> */}
+            <p>
+              “Awakening paths: Exploring psychology as a career” - PAN India
+              online session
+            </p>
             <button className="work-button">
               <IoArrowRedoCircleSharp className="work-icon" />
             </button>
