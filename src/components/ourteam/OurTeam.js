ff682616480fdbd6ae8fdff95efea7959040f123
saurabh021120 <saurabhratnaparkhi11@gmail.com>
2021-08-18 18:12:28 +0530

    Final theme

--- src/components/ourteam/OurTeam.js
+++ src/components/ourteam/OurTeam.js
@@ -7,6 +7,9 @@ import Member from "./y1.jpeg";
 import Member2 from "./y2.jpeg";
 import Member3 from "./y3.jpeg";
 
+import BOT1 from "./BOT1.jpeg";
+import BOT2 from "./BOT2.jpeg";
+
 import Card from "./Card";
 
 import { useInView } from "react-intersection-observer";
@@ -149,7 +152,7 @@ function OurTeam() {
           }}
           className="secondary-heading"
         >
-          Other Advisors
+          Board of Therapists
         </motion.h2>
 
         <motion.div
@@ -159,7 +162,7 @@ function OurTeam() {
           initial="hidden"
           animate={animation4}
         >
-          <Card
+          {/* <Card
             member={Member}
             name="Vikasni Kannan"
             designation="Clinical Psychologist"
@@ -180,6 +183,22 @@ function OurTeam() {
             designation="Counsellor & Soft skills trainer"
             headline="Monika Bagri is a dynamic & energetic trainer with the latest flair in psychology and soft skills."
             description="She is an Engineer and has done post-graduation in Psychology. She has counselled many students and adults on one on one basis aa well in group setting. Apart from this, she has delivered assignments on life skills, soft skills, time management and communication skills to many Corporates, Educational / Coaching institutes and NGOs."
+          /> */}
+
+          <Card
+            member={BOT1}
+            name="Japneet Anand"
+            designation="Counsellor"
+            headline="Japneet Anand is the founder of Awaken: Positive Psychological Approach to Mental Health and Training."
+            description="Fourteen years of work experience conducting workshops and training many people in educational institutes, NGOs, corporate and Youth groups made Mrs Anand an exemplary professional. Her work proves that he is as passionate about training as he is for one on one consultations. Mrs Anand's passion for the job has guided her in understanding the wholesomeness in helping. Her forte is his determination to mentor people towards positivity, change, and balance in life through counselling and coaching. Mrs Anand, through various psychological techniques, also helps the individual pave their way towards a deeper understanding of their life at large."
+          />
+
+          <Card
+            member={BOT2}
+            name="Manasi Brahme"
+            designation="Counsellor"
+            headline="Manasi Brahme is a highly motivated Industrial Psychologist who is skilled and experienced in various fields.Her experience in the HR field of different organisations deepened her understanding of human behaviour. "
+            description="Ms Brahme worked in almost every branch of the HR industry, which is why her contributions towards employee development in an organisation are exceptional. Her expertise in Psychometry, public speaking and leadership, polished her career through the years. Ms Brahme worked with NGOs as a Psychology Volunteer and constructed questionnaires and data analysis sheets. Her certification and proficiency in German make her commendable in language training. Ms Brahme is a dedicated worker. She is always willing to work her fullest to make the organisation better for employees."
           />
         </motion.div>
       </div>
