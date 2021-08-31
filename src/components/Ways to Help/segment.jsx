14f81312f688988d241460eac3d3956bd9cb3469
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-09-01 00:57:20 +0530

    reach section svg added

--- src/components/Ways to Help/segment.jsx
+++ src/components/Ways to Help/segment.jsx
@@ -1,80 +1,46 @@
 import React from "react";
 import "./segment.css";
-import c from "./Images/c.png";
-import b from "./Images/b.png";
-import f from "./Images/f.png";
-import mm from "./Images/mm.png";
+import share from "./Images/share.svg";
+import interact from "./Images/interact.svg";
+import connect from "./Images/connect.svg";
+import unleash from "./Images/unleash.svg";
+import { FiArrowRightCircle } from "react-icons/fi";
 
 function Segment() {
   return (
-    <div class="main_body">
-      <div>
-        <h1 className="primary-heading"> REACH OUT TO US </h1>
-      </div>
-      <div class="lowerpart">
-        <div class="innerbox">
-          <div class="icon 1st">
-            <img className="imgsegcss" src={c}></img>
+    <div class="reach-out">
+      {/* <h1>INTERACT WITH OUR REPRESENTATIVE</h1>
+      <h1>SHARE YOUR CONCERN</h1>
+      <h1>CONNECT TO OUR EXPERTS</h1>
+      <h1>UNLEASH A BETTER YOU</h1> */}
+      <h1 className="primary-heading">Reach out to us</h1>
+
+      <div className="reach-out-points">
+        <div className="reach-point">
+          <div className="reach-out-point">
+            <img src={interact} />
+            <h2>interact with our representatives</h2>
           </div>
-          <div class="para">
-            <div class="heading">
-              <h1>INTERACT WITH OUR REPRESENTATIVE</h1>
-            </div>
-            {/* <div class="rest">
-              <p>
-                Chat anonymously with a Your Friend representative who is here
-                to help you with judging you
-              </p>
-            </div> */}
-          </div>
+          <FiArrowRightCircle class="reach-out-arrow" />
         </div>
-        <div class="innerbox">
-          <div class="icon 2nd">
-            <img className="imgsegcss" src={mm}></img>
+        <div className="reach-point">
+          <div className="reach-out-point">
+            <img src={share} />
+            <h2>share your concern</h2>
           </div>
-          <div class="para">
-            <div class="heading">
-              <h1>SHARE YOUR CONCERN</h1>
-            </div>
-            {/* <div class="rest">
-              <p>
-                Open up to your representative in a sapce where you get the
-                guidance you need,and your concerns get the attention they
-                derserve
-              </p>
-            </div> */}
-          </div>
+          <FiArrowRightCircle class="reach-out-arrow" />
         </div>
-        <div class="innerbox">
-          <div class="icon 3rd">
-            <img className="imgsegcss" src={b}></img>
+        <div className="reach-point">
+          <div className="reach-out-point">
+            <img src={connect} />
+            <h2>connect to our experts</h2>
           </div>
-          <div class="para">
-            <div class="heading">
-              <h1>CONNECT TO OUR EXPERTS</h1>
-            </div>
-            {/* <div class="rest">
-              <p>
-                With 24*7 support from our team, get connected to the right
-                therapist for yourself
-              </p>
-            </div> */}
-          </div>
+          <FiArrowRightCircle class="reach-out-arrow" />
         </div>
-        <div class="innerbox">
-          <div class="icon 4th">
-            <img className="imgsegcss" src={f} alt=""></img>
-          </div>
-          <div class="para">
-            <div class="heading">
-              <h1>UNLEASH A BETTER YOU</h1>
-            </div>
-            {/* <div class="rest">
-              <p>
-                Our aim at Your Friend is to make you a healthy and better
-                version of yourself
-              </p>
-            </div> */}
+        <div className="reach-point">
+          <div className="reach-out-point">
+            <img src={unleash} />
+            <h2>unleash a better you</h2>
           </div>
         </div>
       </div>
