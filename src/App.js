eaa0e9c11c780620a7d33f9ca3d0f72285065468
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2021-09-21 16:48:30 +0530

    structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
-import OurTeam from "./components/ourteam/OurTeam";
-import Aboutus from "./components/Aboutus/Aboutus";
-import Campaign from "./components/Campaign/Campaign";
-import Webinar from "./components/Webinar/Webinar";
-import Internship from "./components/Internship/Internship.jsx";
-// import Campaign from "./components/Campaign/Campaign";
-import Ourteam from "./components/ourteam/OurTeam";
-import aboutus from "./components/Aboutus/Aboutus";
-// import Webinar from "./components/Webinar/Webinar";
+import Home from "../src/pages/home/Home";
 
 function App() {
-  return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/campaign" component={Campaign} />
-      <Route exact path="/ourteam" component={Ourteam} />
-      <Route exact path="/about-us" component={aboutus} />
-      <Route exact path="/webinars" component={Webinar} />
-      <Route exact path="/internship" component={Internship} />
-    </Router>
-  );
+  return <Home />;
 }
 
 export default App;
