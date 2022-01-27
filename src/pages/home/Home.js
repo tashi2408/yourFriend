fd8ebd8243f0f1715e390e48744121a9e7a7dffa
Shivom Srivastava <shvomsrivastava@gmail.com>
2022-01-27 14:24:03 +0530

    New Home Component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,50 @@
-import React from "react";
-
 import "./home.css";
-
+import Logo from "./logo.png";
 export default function Home() {
   return (
     <>
-      <h1>HOME WORLD</h1>
+      <header>
+        <nav>
+          <div className="wrapper">
+            <img
+              className="logo"
+              alt="your friend logo"
+              src={Logo}
+              height={70}
+              width={70}
+            />
+            <ul className="links">
+              <li className="linkItem">
+                <a href="/#">About</a>
+              </li>
+              <li className="linkItem">
+                <a href="/#">Join</a>
+              </li>
+              <li className="linkItem">
+                <a href="/#">Events</a>
+              </li>
+              <li className="linkItem">
+                <a href="/#">Chat</a>
+              </li>
+              <li className="btn">
+                <a href="/#">Login</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="content">
+          <div className="wrapper">
+            <h1>Your Friend</h1>
+            <p>A Mental Health Initiative by Youth India Foundation</p>
+            <div className="btn-group">
+              <a href="/#" className="btn">Join us</a>
+              <a href="/#" className="btn">
+                Book a Session
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
