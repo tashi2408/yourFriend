f9bb0e6115463b14bc9c2ad74b0f61d9cf470cf1
Shivom Srivastava <shvomsrivastava@gmail.com>
2022-02-02 06:45:10 +0000

    💄 🩹 homepage blog component fix

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -2,63 +2,12 @@ import { useParams, Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import './blog.css';
+import Posts from '../../posts';
 import { BsFacebook } from 'react-icons/bs';
 import { IoLogoWhatsapp } from 'react-icons/io';
-import { AiFillInstagram } from 'react-icons/ai';
 
 const Blog = () => {
   const { id } = useParams();
-  const Posts = [
-    {
-      id: 1,
-      title: 'Sample Title 1',
-      markdown: `
-  # This is, A *Sample* Title 1
-  #### This is a __Sample__ SubTitle
-  This is a very nice Sample ~post~.
-  Sample Link: https://shivom.me
-      `,
-      image: '//placekitten.com/g/800/300',
-    },
-
-    {
-      id: 2,
-      title: 'Sample Title 2',
-      markdown: `
-  # This is, A *Sample* Title 2
-  #### This is a __Sample__ SubTitle
-  This is a very nice Sample ~post~.
-  Sample Link: https://shivom.me
-      `,
-      image: '//placekitten.com/g/800/300',
-    },
-    {
-      id: 3,
-      title: 'Sample Title 3',
-      markdown: `
-  # This is, A *Sample* Title 3
-  #### This is a __Sample__ SubTitle
-  This is a very nice Sample ~post~.
-  Sample Link: https://shivom.me
-      `,
-      image: '//placekitten.com/g/800/300',
-    },
-    {
-      id: 4,
-      title: 'Sample Title 4',
-      markdown: `
-  # This is, A *Sample* Title 4
-  ### This is a __Sample__ SubTitle
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt aut eius quas tenetur inventore ex dolores maiores pariatur, 
-  eos hic optio assumenda voluptatem dignissimos consequuntur et magni, tempore asperiores aspernatur. 
-  Perspiciatis eveniet error ut quas necessitatibus doloremque id officia dolorum! 
-  Ipsa voluptas necessitatibus provident numquam asperiores vel facilis accusamus aut deleniti 
-  ### This is a __Sample__ SubTitle
-  magni sunt velit saepe sequi repellendus sapiente, natus est. Iste, architecto, optio expedita, aspernatur neque sint cumque ipsam in nulla ipsum vel fugit. Vel nihil ipsa atque rerum numquam? Eius delectus, quidem cum, voluptatibus assumenda, atque quibusdam culpa incidunt omnis eligendi a eum magni distinctio velit! Dolores, labore accusantium itaque architecto consequatur magnam exercitationem ut ea necessitatibus quasi atque reprehenderit, deleniti optio. Maxime, esse. Magni quo recusandae est! Autem qui repellat, dolorem illo natus rerum possimus deleniti consequuntur similique ducimus maiores consectetur laboriosam earum officia! Nostrum, iure illum? Nisi ab laudantium dolorum id dolor culpa cumque repudiandae sequi nemo vitae dolorem error odit incidunt minima, distinctio repellendus in odio adipisci optio iusto iste est totam! Omnis obcaecati consectetur distinctio, dicta, autem dignissimos ex nemo itaque magnam officia maiores perspiciatis animi maxime harum voluptate facere possimus modi quia iure unde. Animi, voluptas. Nam veritatis facere consectetur totam voluptatem ut at odio a? Commodi repellendus adipisci quo eos, omnis minus placeat voluptatibus temporibus nostrum, reprehenderit asperiores odio odit blanditiis sed ratione. Nisi optio corporis molestiae molestias minima, ipsum quisquam repellendus rerum error esse! Labore eaque illum quas? Beatae molestiae exercitationem quia. Accusamus voluptatem quis laudantium delectus unde? Magnam pariatur debitis recusandae, dolores reprehenderit eaque nostrum possimus quae autem mollitia porro aperiam, nisi impedit maiores laboriosam inventore qui. Cum minima quis optio, porro animi reprehenderit suscipit consectetur tempore autem sed, neque illum at tempora quibusdam laudantium saepe accusantium, dicta mollitia? A, quas consequuntur ea deleniti esse, dicta facere culpa optio aspernatur officia veniam doloribus porro voluptatibus voluptate obcaecati corporis inventore omnis officiis, eligendi nam. Aspernatur provident sunt placeat distinctio soluta quod totam minima quia ut, sint aperiam laudantium eum corrupti esse ullam omnis. Sunt, vitae? Blanditiis libero at vero perspiciatis. Corrupti perferendis sit quam voluptatem sunt minus excepturi obcaecati modi qui dolor odit illo delectus cum possimus, incidunt fuga nulla repudiandae nobis quibusdam dolorem maiores voluptate blanditiis suscipit! Molestiae rem odit amet suscipit unde iste eum fugiat, temporibus alias magni provident veritatis ab ut laborum quas sunt adipisci repudiandae doloribus possimus est necessitatibus quam atque laudantium illum. Amet ab ipsa sunt, sint repellat doloribus recusandae enim impedit saepe accusamus delectus repudiandae velit, magni ducimus veniam molestias eveniet quibusdam porro. Obcaecati dolores nostrum, delectus atque excepturi illo alias sapiente tenetur harum earum commodi quibusdam, iusto fugit, voluptatum aperiam impedit veniam enim ratione deleniti aliquid. Velit, dicta dolore iure, officia earum rerum cumque sed aspernatur quo nulla eveniet pariatur autem nostrum architecto, debitis commodi ipsa ducimus minus? Rem nemo aliquid ut minima expedita consequatur libero similique repellendus aspernatur voluptatem perferendis atque excepturi magnam perspiciatis ullam adipisci, veniam deserunt et. Natus iure dolor dolorem sequi iusto quas illum sit, consequatur, adipisci iste eum laboriosam inventore enim minima sunt dicta saepe molestiae, et repellendus nisi. Ad ipsam ab eius commodi impedit repellat qui quidem temporibus tempore delectus vitae, aspernatur maxime quo eaque porro? Laboriosam harum nam tempore quasi unde sequi, eos ad. Voluptas corporis quidem quis ex aspernatur eveniet omnis soluta, sequi tenetur quibusdam exercitationem? Dolorum obcaecati quisquam sequi cumque consectetur beatae id natus, recusandae omnis dolorem nam iusto assumenda temporibus. Earum dolorem necessitatibus praesentium labore? Quidem, commodi at. Vitae laborum voluptatem quisquam iste velit sapiente reiciendis explicabo reprehenderit in eaque nesciunt sunt, id labore qui perspiciatis ut nobis quidem hic aliquam recusandae voluptas? Sint consequuntur veritatis eum consectetur repellendus ullam qui, eligendi amet, aspernatur maxime earum soluta enim esse atque optio inventore corrupti veniam! Corrupti at exercitationem hic impedit deserunt. Accusamus repellendus ad sunt a, voluptatem eaque nihil, tenetur minus animi magnam earum eius deleniti, non debitis iure accusantium molestias? A ad praesentium corporis explicabo esse, voluptatem iste omnis illo cum neque tempora eligendi distinctio sed natus nostrum rerum? Odio sapiente corrupti omnis labore reprehenderit pariatur ab illo quia consequuntur corporis, nesciunt quidem voluptatum voluptates aspernatur quo, temporibus alias exercitationem ut facilis nisi, adipisci rerum. Amet eum in fugiat nemo nam corporis nisi hic, similique odio facere placeat doloribus minima, rem neque eaque ut doloremque officia voluptatibus voluptas accusantium quasi sit. Voluptas fugit autem veniam modi, inventore ullam reprehenderit distinctio amet ipsa, porro esse quisquam ratione beatae commodi dolor consectetur tempora optio alias excepturi nulla deleniti cumque repellendus atque. Fugit expedita, veniam quod placeat, officia beatae iste unde aliquid voluptatem enim facere sed id molestias quae doloribus! Esse qui excepturi asperiores aperiam ipsam deleniti earum nihil! Aperiam voluptatibus ad voluptatem! Perspiciatis possimus consequatur dolorum fugiat odio temporibus, molestias velit maxime autem, harum minus facere accusamus eius voluptatibus tenetur expedita maiores perferendis. Iste cum consequuntur esse! Labore possimus libero, ad facilis reiciendis, dolorem enim tempora quod excepturi autem vel impedit! Ullam optio architecto magnam nostrum corporis veritatis ratione, sunt eligendi minima corrupti voluptatibus deserunt neque quod, minus laborum. Quidem atque vel nulla quaerat delectus necessitatibus nihil placeat inventore? Iusto id non recusandae suscipit culpa illo libero modi nemo aliquid ratione repellat, corporis rerum reprehenderit, iste hic impedit quaerat fugiat ipsum, veritatis pariatur. Officiis voluptas quos cumque mollitia adipisci suscipit obcaecati sunt explicabo vero, dignissimos alias consequatur inventore harum quia iste nesciunt nam placeat aperiam distinctio, praesentium iusto neque! Nesciunt incidunt tenetur repellat, alias consectetur quos explicabo illum dicta pariatur ex non perspiciatis corporis sit eligendi libero repellendus commodi nihil sequi ad optio. Unde nobis fugit sint laboriosam, quaerat odio enim cum? Earum vel necessitatibus temporibus esse non reprehenderit reiciendis, illo placeat dolore mollitia incidunt sit nostrum maiores doloremque expedita nisi vitae. Dolor inventore architecto libero ex accusamus cum qui fugit magni eaque earum quo, non assumenda quaerat quia eos distinctio saepe! Natus nam illum voluptates officia quam sapiente cumque. Voluptates, illo accusamus? Nihil aperiam earum sit, odio ipsum laborum perspiciatis, debitis magni officia deserunt impedit eaque rerum? Iste nemo, quaerat autem delectus odit quam eaque quisquam.
-      `,
-      image: '//placekitten.com/g/800/300',
-    },
-  ];
   const Found = Posts.find((post) => post.id == id);
   const NotFound = `
   # Sorry, We couldn't find that post
--- src/pages/home/Blog/Blog.jsx
+++ src/pages/home/Blog/Blog.jsx
@@ -1,97 +1,46 @@
-import React from 'react';
 import './Blog.css';
-import one from '../../../assets/blog/one.jpg';
-import two from '../../../assets/blog/two.jpg';
-import three from '../../../assets/blog/three.jpg';
-import four from '../../../assets/blog/four.jpg';
+import Posts from '../../../posts';
+import { Link } from 'react-router-dom';
 
-function Blog() {
+const Blog = () => {
   return (
     <div className="blogmaincss">
-      <div class="container">
+      <div class="wrapper">
         <h1 className="primary-heading ">Latest on Your Friend Blog</h1>
-        {/* <p>
-            In this section we are providing the latest update,blog and
-            informative article which may help so do check it out!
-          </p> */}
         <main>
-          <div class="singleBlog">
-            <img src={one} alt=""></img>
-            <div class="blogContent">
-              <h3>
-                Psychological Impact of The life during Covid-19
-                <span>Article</span>
-              </h3>
-              <p>
-                Impact<a href="#">by covid 19</a> | <a href="#">Insight</a>
-              </p>
-              <a href="#" class="btn">
-                Read More
-              </a>
+          {Posts.map((post) => (
+            <div class="singleBlog" key={post.id}>
+              <img src={post.image} alt={post.title}></img>
+              <div class="blogContent">
+                <h3>
+                  {post.title}
+                  <span>Article</span>
+                </h3>
+                {/* <p>
+                  Impact<a href="/">by covid 19</a> | <a href="/">Insight</a>
+                </p> */}
+                <Link to={`/blog/${post.id}`} className="btn">
+                  Read More
+                </Link>
+              </div>
             </div>
-          </div>
-          <div class="singleBlog">
-            <img src={two} alt=""></img>
-            <div class="blogContent">
-              <h3>
-                Choose therapy over pain & Overcome fear <span>Article</span>
-              </h3>
-              <p>
-                By <a href="#">SUHANI SUMAN</a> | <a href="#">Homepage</a>
-              </p>
-              <a href="#" class="btn">
-                Read More
-              </a>
-            </div>
-          </div>
-          <div class="singleBlog">
-            <img src={three} alt=""></img>
-            <div class="blogContent">
-              <h3>
-                Counselors never tell you what to do,They help guide you{' '}
-                <span>Article</span>
-              </h3>
-              <p>
-                6 Ways to distinguish <a href="#">Counselling</a> |{' '}
-                <a href="#">Homepage</a>
-              </p>
-              <a href="#" class="btn">
-                Read More
-              </a>
-            </div>
-          </div>
-          <div class="singleBlog">
-            <img src={four} alt=""></img>
-            <div class="blogContent">
-              <h3>
-                Support them and be there for them through ther worst time
-                <span>Article</span>
-              </h3>
-              <p>
-                5 Tips to Encouraged <a href="#">the loved ones</a> |{' '}
-                <a href="#">Family and Friends</a>
-              </p>
-              <a href="#" class="btn">
-                Read More
-              </a>
-            </div>
-          </div>
+          ))}
         </main>
         <div
           style={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-            paddingTop: '10px',
+            paddingTop: '30px',
           }}
         >
-          <a href="#" className="button pulse">
+          <Link to="/blog" className="btn">
             Read more
-          </a>
+          </Link>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Blog;
