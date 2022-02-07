68a5a1ab8352e2e3d5ef84ff00f9c1c5b7a52f3a
Shivom Srivastava <shvomsrivastava@gmail.com>
2022-02-07 05:59:44 +0000

    💩 layout change

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,4 @@
 import './home.css';
-import Handshake from '../../assets/handshake.svg';
 import Help from '../../assets/help.svg';
 import Overview from './overview/Overview';
 import Contact from './contactUs/ContactUs';
@@ -14,26 +13,24 @@ const Home = () => {
   return (
     <>
       <header>
-        <div className="content">
-          <div className="wrapper">
-            <div class="left">
-              <h1>Your Friend</h1>
-              <p>
-                A Mental Health Initiative by <br />{' '}
-                <span className="yif">Youth India Foundation</span>
-              </p>
-              <div className="btn-group">
-                <Link to="/" className="btn">
-                  Join us
-                </Link>
-                <Link to="/" className="btn">
-                  Book a Session
-                </Link>
-              </div>
+        <div className="wrapper">
+          <div class="left">
+            <h1>Your Friend</h1>
+            <p>
+              A Mental Health Initiative by <br />{' '}
+              <span className="yif">Youth India Foundation</span>
+            </p>
+            <div className="btn-group">
+              <Link to="/" className="btn">
+                Join us
+              </Link>
+              <Link to="/" className="btn">
+                Book a Session
+              </Link>
             </div>
-            <div class="right">
-              <img src={Help} alt="brain" draggable={false} />
-            </div>
+          </div>
+          <div class="right">
+            <img src={Help} alt="brain" draggable={false} />
           </div>
         </div>
       </header>
