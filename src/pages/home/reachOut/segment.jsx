964cf60aa31639a72cb07d8eb1e071d52d644ee0
Shivom Srivastava <shvomsrivastava@gmail.com>
2022-02-11 04:34:33 +0000

    💄 remove arrows from reach out

--- src/pages/home/reachOut/segment.jsx
+++ src/pages/home/reachOut/segment.jsx
@@ -1,45 +1,37 @@
-import React from "react";
-import "./segment.css";
-import share from "./Images/share.svg";
-import interact from "./Images/interact.svg";
-import connect from "./Images/connect.svg";
-import unleash from "./Images/unleash.svg";
-import { FiArrowRightCircle } from "react-icons/fi";
+import React from 'react';
+import './segment.css';
+import share from './Images/share.svg';
+import interact from './Images/interact.svg';
+import connect from './Images/connect.svg';
+import unleash from './Images/unleash.svg';
+import { FiArrowRightCircle } from 'react-icons/fi';
 
 function Segment() {
   return (
-    <div class="reach-out">
-      {/* <h1>INTERACT WITH OUR REPRESENTATIVE</h1>
-      <h1>SHARE YOUR CONCERN</h1>
-      <h1>CONNECT TO OUR EXPERTS</h1>
-      <h1>UNLEASH A BETTER YOU</h1> */}
+    <div class="wrapper">
       <h1 className="primary-heading">Reach out to us</h1>
-
       <div className="reach-out-points">
         <div className="reach-point">
           <div className="reach-out-point">
-            <img src={interact} />
+            <img src={interact} alt="interact" />
             <h2>interact with our representatives</h2>
           </div>
-          <FiArrowRightCircle class="reach-out-arrow" />
         </div>
         <div className="reach-point">
           <div className="reach-out-point">
-            <img src={share} />
+            <img src={share} alt="share" />
             <h2>share your concern</h2>
           </div>
-          <FiArrowRightCircle class="reach-out-arrow" />
         </div>
         <div className="reach-point">
           <div className="reach-out-point">
-            <img src={connect} />
+            <img src={connect} alt="connect" />
             <h2>connect to our experts</h2>
           </div>
-          <FiArrowRightCircle class="reach-out-arrow" />
         </div>
         <div className="reach-point">
           <div className="reach-out-point">
-            <img src={unleash} />
+            <img src={unleash} alt="unleash" />
             <h2>unleash a better you</h2>
           </div>
         </div>
