71e46c5ebe690feb501faa2e8cc1469fc4269d8d
Shivom Srivastava <shvomsrivastava@gmail.com>
2022-02-12 11:46:33 +0000

    💄 💩 footer changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,47 +6,59 @@ import {
   FaLinkedinIn,
 } from 'react-icons/fa';
 
+import Links from '../../socialLinks';
+
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer-content">
-        <div className="social-medialinks">
-          <ul className="footer-list">
-            <li className="footer-list-item">
-              <a className="footer-list-link" href="#">
-                <FaFacebookF className="fab footer-icon" />{' '}
-              </a>
-            </li>
-            <li className="footer-list-item">
-              <a className="footer-list-link" href="#">
-                <FaTwitter className="fab footer-icon" />{' '}
-              </a>
-            </li>
-            <li className="footer-list-item">
-              <a className="footer-list-link" href="#">
-                <FaLinkedinIn className="fab footer-icon" />{' '}
-              </a>
-            </li>
-            <li className="footer-list-item">
-              <a className="footer-list-link" href="#">
-                <FaInstagram className="fab footer-icon" />{' '}
-              </a>
-            </li>
-          </ul>
+    <footer className="footer">
+      <div class="wrapper">
+        <div className="footer-content">
+          <div class="footer-left">
+            <div className="social-medialinks">
+              <ul className="footer-list">
+                <li className="footer-list-item">
+                  <a className="footer-list-link" href={Links[0].url}>
+                    <FaFacebookF className="fab footer-icon" />{' '}
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a className="footer-list-link" href={Links[0].url}>
+                    <FaTwitter className="fab footer-icon" />{' '}
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a className="footer-list-link" href={Links[0].url}>
+                    <FaLinkedinIn className="fab footer-icon" />{' '}
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a className="footer-list-link" href={Links[0].url}>
+                    <FaInstagram className="fab footer-icon" />{' '}
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="links-footer">
+              <p className="link-item">Privacy Policy</p>
+              <p className="link-item">|</p>
+              <p className="link-item">Report a Problem</p>
+              <p className="link-item">|</p>
+              <p className="link-item">FAQ</p>
+              <p className="link-item">|</p>
+              <p className="link-item">Testimonials</p>
+            </div>
+          </div>
+          <div className="bae">
+            <h3>Become an Expert</h3>
+            <p>Listen to others & be their friend in need</p>
+            <a href="/" className="btn">
+              Become an Expert
+            </a>
+          </div>
         </div>
-        <div className="links-footer">
-          <p className="link-item">Privacy Policy</p>
-          <p className="link-item">|</p>
-          <p className="link-item">Report a Problem</p>
-          <p className="link-item">|</p>
-          <p className="link-item">FAQ</p>
-          <p className="link-item">|</p>
-          <p className="link-item">Testimonials</p>
-        </div>
+        <div className="legal">&copy; Youth India Foundation</div>
       </div>
-
-      <div className="legal">&copy; Youth India Foundation</div>
-    </div>
+    </footer>
   );
 };
 
