5e2a5ba80c3a2550e1de59fd5ece1708c0f4857b
Shivom Srivastava <shvomsrivastava@gmail.com>
2022-02-17 13:14:26 +0000

    💄 footer w/ about section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,40 +12,19 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div class="wrapper">
-        <div className="footer-content">
+        <div className="footer-content wrapper">
           <div class="footer-left">
-            <div className="social-medialinks">
-              <ul className="footer-list">
-                <li className="footer-list-item">
-                  <a className="footer-list-link" href={Links[0].url}>
-                    <FaFacebookF className="fab footer-icon" />{' '}
-                  </a>
-                </li>
-                <li className="footer-list-item">
-                  <a className="footer-list-link" href={Links[0].url}>
-                    <FaTwitter className="fab footer-icon" />{' '}
-                  </a>
-                </li>
-                <li className="footer-list-item">
-                  <a className="footer-list-link" href={Links[0].url}>
-                    <FaLinkedinIn className="fab footer-icon" />{' '}
-                  </a>
-                </li>
-                <li className="footer-list-item">
-                  <a className="footer-list-link" href={Links[0].url}>
-                    <FaInstagram className="fab footer-icon" />{' '}
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="links-footer">
-              <p className="link-item">Privacy Policy</p>
-              <p className="link-item">|</p>
-              <p className="link-item">Report a Problem</p>
-              <p className="link-item">|</p>
-              <p className="link-item">FAQ</p>
-              <p className="link-item">|</p>
-              <p className="link-item">Testimonials</p>
+            <div class="about-yf">
+              <h3>about your friend</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta
+                eaque, vero itaque maiores vitae aliquid cupiditate sapiente
+                adipisci nam ea inventore perferendis eligendi nulla quis,
+                quidem numquam id rerum eius laborum commodi quo! Adipisci iste
+                tempore soluta, laboriosam repudiandae vitae possimus quo
+                eveniet voluptatibus neque magnam, voluptas placeat fugiat
+                cumque?
+              </p>
             </div>
           </div>
           <div className="bae">
@@ -56,7 +35,42 @@ const Footer = () => {
             </a>
           </div>
         </div>
-        <div className="legal">&copy; Youth India Foundation</div>
+        <div class="footer-bottom">
+          <div className="social-medialinks">
+            <ul className="footer-list">
+              <li className="footer-list-item">
+                <a className="footer-list-link" href={Links[0].url}>
+                  <FaFacebookF className="fab footer-icon" />{' '}
+                </a>
+              </li>
+              <li className="footer-list-item">
+                <a className="footer-list-link" href={Links[0].url}>
+                  <FaTwitter className="fab footer-icon" />{' '}
+                </a>
+              </li>
+              <li className="footer-list-item">
+                <a className="footer-list-link" href={Links[0].url}>
+                  <FaLinkedinIn className="fab footer-icon" />{' '}
+                </a>
+              </li>
+              <li className="footer-list-item">
+                <a className="footer-list-link" href={Links[0].url}>
+                  <FaInstagram className="fab footer-icon" />{' '}
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="links-footer">
+            <p className="link-item">Privacy Policy</p>
+            <p className="link-item">|</p>
+            <p className="link-item">Report a Problem</p>
+            <p className="link-item">|</p>
+            <p className="link-item">FAQ</p>
+            <p className="link-item">|</p>
+            <p className="link-item">Testimonials</p>
+          </div>
+          <div className="legal">&copy; Youth India Foundation</div>
+        </div>
       </div>
     </footer>
   );
