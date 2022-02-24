0e135c401471b63908cfd0b34f9425b764a92693
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2022-02-24 20:04:39 +0530

    experts section template done

--- src/dashboard/students/Experts.js
+++ src/dashboard/students/Experts.js
@@ -11,7 +11,16 @@ export default function Experts() {
         <h1 className="experts_title">What do you want to discuss about? </h1>
       </div>
       <div className="experts_section">
-        <div className="experts_category"></div>
+        <div className="experts_category">
+          <div className="experts_dropdown">
+            <button className="experts_dropbtn">Category</button>
+            <div className="experts_dropdown-content">
+              <button href="#">Link 1</button>
+              <button href="#">Link 2</button>
+              <button href="#">Link 3</button>
+            </div>
+          </div>
+        </div>
         <div className="experts_rating"></div>
 
         <div className="experts_search"></div>
