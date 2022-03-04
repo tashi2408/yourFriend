6e6a8767c2979148467a71f373f7a9710efbd915
manojxk <sdemanojkumar@gmail.com>
2022-03-04 15:39:15 +0530

    Added database

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,15 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const path = require('path');
-const { OAuth2Client } = require('google-auth-library');
+const dotenv = require('dotenv').config();
 
-dotenv.config();
-const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+connectDB();
 
 const app = express();
 app.use(express.json());
 
-const users = [];
-console.log(users);
-function upsert(array, item) {
-  const i = array.findIndex((_item) => _item.email === item.email);
-  if (i > -1) array[i] = item;
-  else array.push(item);
-  console.log(users);
-}
-
-app.post('/api/google-login', async (req, res) => {
-  const { token } = req.body;
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: process.env.CLIENT_ID,
-  });
-  const { name, email, picture } = ticket.getPayload();
-  upsert(users, { name, email, picture });
-  res.status(201);
-  res.json({ name, email, picture });
-});
+app.use('/api/users', require('./routes/userRoutes'));
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const {
-    registerUser,
-    loginUser,
-    getMe,
-} = require('../controllers/userController')
-const { protect } = require('../middleware/authMiddleware')
+  registerUser,
+  loginUser,
+  getMe,
+  googleLogin,
+} = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 
-router.post('/', registerUser)
-router.post('/login', loginUser)
-router.get('/me', protect, getMe)
+router.post('/', registerUser);
+router.post('/google-login', googleLogin);
+router.post('/login', loginUser);
+router.get('/me', protect, getMe);
 
-module.exports = router
+module.exports = router;
