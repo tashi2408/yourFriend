4350f79e942f82ee32e0e7cc7e1f7b5069195fa8
manoj <112118034@nitt.edu>
2022-03-17 15:26:52 +0530

    Added Profile UI with backend

--- src/dashboard/students/Profile.js
+++ src/dashboard/students/Profile.js
@@ -1,33 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './Profile.css';
 import logo from "./profile.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserEdit } from '@fortawesome/free-solid-svg-icons';
 import { margin } from "@mui/system";
 function Profile() {
-  return (
-    <div className="Profile flex-center">
-        <div className={"background flex-center"}>
-            <img className={"img"} alt="oops" src={logo}></img>
-        </div>
-        <div className="infocard">
-            <p className="username">Username</p>
-            <p className="mail-id">Mail-Id</p>
-        </div>
-        <div className="detailscard" >
-            <div className="edit"><h2>Personal Details</h2><a style={{ float:"right"}} ><FontAwesomeIcon icon={faUserEdit}/></a></div>
-            <div className="dateofbirth"><label for="test">Date Of Birth :</label>
-            <span><input name="dob" id="data" type="text" /></span>
+    return (
+        <div className="Profile flex-center">
+            <div className={"background flex-center"}>
+                <img className={"img"} alt="oops" src={JSON.parse(localStorage.getItem('loginData')).picture}></img>
             </div>
-            <div className="occupation"><label for="test">..Occupation :</label>
-            <span><input name="occupation" id="data" type="text" /></span>
+            <div className="infocard">
+                <p className="username">Username: {JSON.parse(localStorage.getItem('loginData')).name}</p>
+                <p className="mail-id">Mail-Id: {JSON.parse(localStorage.getItem('loginData')).email}</p>
             </div>
-            <div className="gender"><label for="test">...........Gender :</label>
-            <span><input name="gender" id="data" type="text" /></span>
+            <div className="detailscard" >
+                <div className="edit"><h2>Personal Details</h2><a style={{ float: "right" }} ><FontAwesomeIcon icon={faUserEdit} /></a></div>
+                <div className="dateofbirth"><label for="test">Date Of Birth :</label>
+                    <span><input name="dob" id="data" type="text" /></span>
+                </div>
+                <div className="occupation"><label for="test">..Occupation :</label>
+                    <span><input name="occupation" id="data" type="text" /></span>
+                </div>
+                <div className="gender"><label for="test">...........Gender :</label>
+                    <span><input name="gender" id="data" type="text" /></span>
+                </div>
             </div>
         </div>
-   </div>
-  );
+    );
 }
 
 export default Profile;
