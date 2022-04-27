b002640d3505dde0d7d19cc2f8e4dcf11f4857ef
Shivom <shvomsrivastava@gmail.com>
2022-04-27 15:40:01 +0530

    💩 Profile page fix

--- src/dashboard/students/Profile.js
+++ src/dashboard/students/Profile.js
@@ -1,27 +1,38 @@
-import React, { useState } from "react";
-import FormikForm from "./FormikForm";
+import React, { useState } from 'react';
+import FormikForm from './FormikForm';
 import './Profile.css';
 
 function Profile() {
-    const [fields, updateFields] = useState({
-        dob: "N/A",
-        occupation: "N/A",
-        gender: "N/A"
-      });
-    return (
-        <div className="Profile flex-center">
-            <div className={"background flex-center"}>
-                <img className={"img"} alt="oops" src={JSON.parse(localStorage.getItem('loginData')).picture}></img>
-            </div>
-            <div className="infocard">
-                <p className="username">{JSON.parse(localStorage.getItem('loginData')).name}</p>
-                <p className="mail-id">{JSON.parse(localStorage.getItem('loginData')).email}</p>
-            </div>
-            <div className="detailscard" >
-                <FormikForm fields={fields} updateFields={updateFields} />
-                </div>
-            </div>
-    );
+  const [fields, updateFields] = useState({
+    dob: 'N/A',
+    occupation: 'N/A',
+    gender: 'N/A',
+  });
+  return (
+    <div className="Profile">
+      <div className="wrapper">
+        <div className="infocard">
+          <div className="profile__img">
+            <img
+              alt="Profile"
+              src={JSON.parse(localStorage.getItem('loginData')).picture}
+            ></img>
+          </div>
+          <div className="infocard__info">
+            <span className="username">
+              {JSON.parse(localStorage.getItem('loginData')).name}
+            </span>
+            <span className="mail-id">
+              {JSON.parse(localStorage.getItem('loginData')).email}
+            </span>
+          </div>
+        </div>
+        <div className="detailscard">
+          <FormikForm fields={fields} updateFields={updateFields} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
